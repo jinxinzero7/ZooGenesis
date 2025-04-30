f96ab6780e780a29b424061cf96f178fbe0c92c9
Николай <jinxinzero7@gmail.com>
2025-04-30 13:27:25 +0300

    Add files via upload
    Создал проект и добавил в него базовые классы животных, а также абстрактный класс Animal

---
+++ ZooGenesis/ZooGenesis/Classes/Lion.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZooGenesis
+{
+    public enum ManeLength
+    {
+        Короткая,
+        Средняя,
+        Большая,
+        Великолепная
+    }
+    public class Lion : Animal
+    {
+        // длина гривы
+        private ManeLength _maneSize;
+        public ManeLength ManeSize
+        {
+            get { return _maneSize; }
+            set { _maneSize = value; }
+        }
+        public Lion(string name, int age, double health,ManeLength maneSize, Gender gender, List<char> genes = null)
+            : base(name, age, health, gender, genes)
+        {
+            ManeSize = maneSize;
+        }
+
+        public override void MakeSound()
+        {
+            Console.WriteLine("Grrr-roar!...");
+        }
+
+        public override string ToString()
+        {
+            return base.ToString() + $", Размер гривы: {ManeSize}";
+        }
+    }
+}
---
+++ ZooGenesis/ZooGenesis/Program.cs
@@ -0,0 +1,5 @@
+﻿using ZooGenesis;
+
+Penguin myPenguin = new Penguin("Король", 5, 100, PenguinSize.Императорский, Gender.Мужской);
+Console.WriteLine(myPenguin); // Вывод информации о пингвине
+myPenguin.MakeSound();
