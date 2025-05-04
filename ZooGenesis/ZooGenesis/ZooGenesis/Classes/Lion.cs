760025ae797f368f626a9c623959dfa9b01b3b0c
Николай <jinxinzero7@gmail.com>
2025-05-04 18:50:10 +0300

    Добавлен новый класс, два интерфейса и метод
    Новый класс Snake, аналогичный двум другим. Два интерфейса IBreedable и IMutable, один в процессе реализации. Добавлен метод Mutate(double mutationRate) в Animal, добавление генов теперь обязательно

---
+++ ZooGenesis/ZooGenesis/ZooGenesis/Classes/Lion.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZooGenesis
+{
+    public enum ManeLength
+    {
+        Маленькая,
+        Средняя,
+        Большая,
+        Великолепная
+    }
+    public class Lion : Animal
+    {
+        // длина гривы
+        private ManeLength _maneSize;
+        public ManeLength ManeSize
+        {
+            get { return _maneSize; }
+            set { _maneSize = value; }
+        }
+        public Lion(string name, int age, double health,ManeLength maneSize, Gender gender, List<char> genes = null)
+            : base(name, age, health, gender, genes)
+        {
+            ManeSize = maneSize;
+            Console.WriteLine($"Создан лев: {name}");
+        }
+
+        public override void MakeSound()
+        {
+            Console.WriteLine("Grrr-roar!...");
+        }
+
+        public override string ToString()
+        {
+            return base.ToString() + $", Размер гривы: {ManeSize}";
+        }
+    }
+}
---
+++ ZooGenesis/ZooGenesis/ZooGenesis/Interfaces/IMutable.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZooGenesis
+{
+    internal interface IMutable
+    {
+        void Mutate(double mutationRate);
+    }
+}
